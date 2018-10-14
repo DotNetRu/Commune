37351e27d2930de3b781bbd1e36317e19f184ee1
Danil Pavlov <d2funlife@gmail.com>
2018-10-14 21:06:45 +0300

    Change build script. Limit docker integration only for master branch

--- build/deploy-docker.ps1
+++ build/deploy-docker.ps1
@@ -1,3 +1,9 @@
+$Branch=$env:APPVEYOR_REPO_BRANCH
+$IsMaster=$Branch -eq "master"
+if (!$IsMaster) {
+    exit 0
+}
+
 Write-Host Starting deploy docker image
 
 $env:DOCKER_PASS | docker login --username dotnetrucd --password-stdin
