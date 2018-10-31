d3c04d0bc50c35cd293964dd1ff7426e4cf5020d
Ekaterina Kuksenok <katerinkakk89@gmail.com>
2018-10-31 12:36:46 +0300

    #46 MeetUp controller + MeetUp application service added some implementations

---
+++ src/MeetupManagement/DotNetRu.MeetupManagement.Application/Services/MeetupDraftService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DotNetRu.Common.Collections;
+using DotNetRu.MeetupManagement.Application.Contract.Exceptions;
+using DotNetRu.MeetupManagement.Application.Contract.Models;
+using DotNetRu.MeetupManagement.Application.Contract.Services;
+using DotNetRu.MeetupManagement.Domain;
+using DotNetRu.MeetupManagement.Domain.Contract.Exceptions;
+
+namespace DotNetRu.MeetupManagement.Application.Services
+{
+    public class MeetupDraftService : IMeetupDraftService
+    {
+        private readonly Domain.Drafts.IMeetupDraftService _meetupDraftService;
+
+        public MeetupDraftService(Domain.Drafts.IMeetupDraftService meetupDraftService)
+        {
+            _meetupDraftService = meetupDraftService ?? throw new ArgumentNullException(nameof(meetupDraftService));
+        }
+
+        public MeetupDraft GetMeetupDraft(string communityId, string meetupId)
+        {
+            var meetupDraft = _meetupDraftService.GetMeetupDraft(new MeetupKey(communityId, meetupId));
+            return MapMeetUpDraft(meetupDraft);
+        }
+
+        public MeetupDraft CreateMeetupDraft(string communityId, string name)
+        {
+            try
+            {
+                // ToDo: description here is missed
+                var meetup = _meetupDraftService.CreateMeetupDraft(communityId, name, string.Empty);
+                return MapMeetUpDraft(meetup);
+            }
+            catch (Exception ex)
+            {
+                if (ex is DomainException)
+                    throw;
+                throw new UnexpectedException("Create talk draft failed.", ex);
+            }
+        }
+
+        public void UpdateMeetupDraft(string communityId, string meetupId, ICollection<string> talksIds, ICollection<string> speakerIds, string venueId)
+        {
+            // ToDo: add update method to domain service or get this meetup and check one by one all the parameters changes?
+            // _meetupDraftService.UpdateMeetupDraft(meetupId);
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMeetupDraft(string communityId, string meetupId)
+        {
+            // ToDo: add delete method to doamin service
+            // _meetupDraftService.RemoveMeetupDraft(meetupId);
+        }
+
+        public void SetVenue(string communityId, string meetupId, string venueId)
+        {
+            _meetupDraftService.SetVenueReference(new MeetupKey(communityId, meetupId), venueId);
+        }
+
+        public void AddFriend(string communityId, string meetupId, string friendId)
+        {
+            _meetupDraftService.AddFriendReference(new MeetupKey(communityId, meetupId), friendId);
+        }
+
+        public void DeleteFriend(string communityId, string meetupId, string friendId)
+        {
+            _meetupDraftService.RemoveFriendReference(new MeetupKey(communityId, meetupId), friendId);
+        }
+
+        public void AddTalkDraft(string communityId, string meetupId, string talkDraftId)
+        {
+            _meetupDraftService.AddTalkDraft(new MeetupKey(communityId, meetupId), talkDraftId);
+        }
+
+        public void DeleteTalkDraft(string communityId, string meetupId, string talkDraftId)
+        {
+            _meetupDraftService.RemoveTalkDraft(new MeetupKey(communityId, meetupId), talkDraftId);
+        }
+
+        private MeetupDraft MapMeetUpDraft(Domain.Drafts.MeetupDraft domainMeetUp)
+        {
+            var mapped = new MeetupDraft(domainMeetUp.Key.Id, domainMeetUp.Key.CommunityId);
+            mapped.Friends.Assign(domainMeetUp.Friends.Select(GetFriendReference));
+
+            // ToDo: map talks references to talks
+            // mapped.Talks.Assign(domainMeetUp.Talks);
+            return mapped;
+        }
+
+        private FriendReference GetFriendReference(EntityReference source)
+        {
+            return new FriendReference(source.Id, source.Name, source.IsDraft);
+        }
+    }
+}
--- src/MeetupManagement/DotNetRu.MeetupManagement.WebApi/Controllers/MeetupController.cs
+++ src/MeetupManagement/DotNetRu.MeetupManagement.WebApi/Controllers/MeetupController.cs
@@ -1,40 +1,81 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using DotNetRu.MeetupManagement.Application.Contract.Services;
 using DotNetRu.MeetupManagement.WebApi.Contract.Controllers;
 using DotNetRu.MeetupManagement.WebApi.Contract.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using FriendReference = DotNetRu.MeetupManagement.WebApi.Contract.Models.FriendReference;
 
 namespace DotNetRu.MeetupManagement.WebApi.Controllers
 {
     public class MeetupController : MeetupApiController
     {
         private readonly ILogger<MeetupController> _logger;
+        private readonly IMeetupDraftService _meetupDraftService;
 
-        // ReSharper disable once UnusedMember.Global
-        public MeetupController(ILogger<MeetupController> logger)
+        public MeetupController(ILogger<MeetupController> logger, IMeetupDraftService meetupDraftService)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _meetupDraftService = meetupDraftService ?? throw new ArgumentNullException(nameof(meetupDraftService));
         }
 
         public override void CreateMeetupDraft([FromRoute, Required] string communityId, [FromBody] CreateMeetupDraftParameters meetupDraft)
         {
-            throw new NotImplementedException();
+            var meetUp = _meetupDraftService.CreateMeetupDraft(communityId, meetupDraft.Name);
+
+            // ToDo: my guess, we need to return meetup id at least
         }
 
         public override void DeleteMeetupDraft([FromRoute, Required] string communityId, [FromRoute, Required] string meetupId)
         {
-            throw new NotImplementedException();
+            _meetupDraftService.DeleteMeetupDraft(communityId, meetupId);
         }
 
-        public override MeetupDraft GetMeetupDraft([FromRoute, Required] string communityId, [FromRoute, Required] string meetupId)
+        public override Contract.Models.MeetupDraft GetMeetupDraft([FromRoute, Required] string communityId, [FromRoute, Required] string meetupId)
         {
-            throw new NotImplementedException();
+            var result = _meetupDraftService.GetMeetupDraft(communityId, meetupId);
+            return Map(result);
         }
 
         public override void UpdateMeetupDraft([FromRoute, Required] string communityId, [FromRoute, Required] string meetupId, [FromBody] UpdateMeetupDraftParameters updateMeetupDraftProperties)
         {
-            throw new NotImplementedException();
+            _meetupDraftService.UpdateMeetupDraft(communityId, meetupId, updateMeetupDraftProperties.TalkIds, updateMeetupDraftProperties.SpeakerIds, updateMeetupDraftProperties.VenueId);
+        }
+
+        private Contract.Models.MeetupDraft Map(Application.Contract.Models.MeetupDraft source)
+        {
+            // ToDo: name and speakers are missing here
+            var destination = new Contract.Models.MeetupDraft { Id = source.Id, Venue = Map(source.Venue) }; // , Friends = new Collection<FriendReference>(), Talks = new Collection<TalkReference>() };
+            source.Friends.Select(f =>
+            {
+                var mapped = Map(f);
+                destination.Friends.Add(mapped);
+                return mapped;
+            });
+            source.Talks.Select(talk =>
+            {
+                var mapped = Map(talk);
+                destination.Talks.Add(mapped);
+                return mapped;
+            });
+            return destination;
+        }
+
+        private Contract.Models.FriendReference Map(Application.Contract.Models.FriendReference source)
+        {
+            return new FriendReference { Id = source.Id, Name = source.Name };
+        }
+
+        private Contract.Models.VenueReference Map(Application.Contract.Models.VenueReference source)
+        {
+            return new Contract.Models.VenueReference { Name = source.Name, Id = source.Id };
+        }
+
+        private TalkReference Map(Application.Contract.Models.TalkDraft source)
+        {
+            return new TalkReference { Id = source.Id, Title = source.Title };
         }
     }
 }
