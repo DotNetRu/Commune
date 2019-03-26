62d329d7b707fe3d4d9f193d4d809ca4e134461e
egorikas <egorgrishechko@gmail.com>
2019-03-26 22:07:45 +0300

    finish removing client removing electron add swagger

--- DevActivator/Controllers/HomeController.cs
+++ DevActivator/Controllers/HomeController.cs
@@ -1,19 +1,12 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevActivator.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-            return View();
+            return "Api works";
         }
     }
 }
