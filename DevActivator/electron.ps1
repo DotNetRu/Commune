9a1855047d4b61215c36d8d4d668306c9bc53956
egorikas <egorgrishechko@gmail.com>
2019-03-26 19:42:33 +0300

    Delete client's app. Step 2

--- DevActivator/electron.ps1
+++
@@ -1,7 +0,0 @@
-invoke-expression '$Env:ASPNETCORE_ENVIRONMENT = "Development"'
-$webpack = Start-Process -FilePath "powershell" -ArgumentList "node node_modules/webpack/bin/webpack.js --watch" -PassThru -NoNewWindow
-$dotnet = Start-Process -FilePath "powershell" -ArgumentList "dotnet electronize start" -PassThru -NoNewWindow
-
-# export ASPNETCORE_ENVIRONMENT="Development"
-# node node_modules/webpack/bin/webpack.js --watch
-# dotnet electronize start
