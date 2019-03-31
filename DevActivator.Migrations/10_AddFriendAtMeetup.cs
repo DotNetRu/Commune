427c3cb7d977b72656eafab9adf6307961e48f68
egorikas <egorgrishechko@gmail.com>
2019-03-31 21:26:34 +0300

    Add data scheme

---
+++ DevActivator.Migrations/10_AddFriendAtMeetup.cs
@@ -0,0 +1,32 @@
+using FluentMigrator;
+
+namespace DevActivator.Migrations
+{
+    [Migration(10)]
+    public class AddFriendAtMeetup : Migration
+    {
+        public override void Up()
+        {
+            var table = Create.Table("FriendAtMeetup");
+            table
+                .WithColumn("MeetupId")
+                .AsInt32()
+                .NotNullable()
+                .ForeignKey("Meetups", "Id");
+            table
+                .WithColumn("FriendId")
+                .AsInt32()
+                .NotNullable()
+                .ForeignKey("Friends", "Id");
+
+            Create.PrimaryKey("FriendAtMeetupId")
+                .OnTable("FriendAtMeetup")
+                .Columns("MeetupId", "FriendId");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("FriendAtMeetup");
+        }
+    }
+}
