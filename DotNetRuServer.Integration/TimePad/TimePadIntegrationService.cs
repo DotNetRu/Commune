8e7d3fa71ae2f619fd08aca4e9068ddcaa916f98
rafaelldi <rival.abdrakhmanov@outlook.com>
2019-10-09 20:43:05 +0300

    Добавил генерацию из razor шаблона

--- DotNetRuServer.Integration/TimePad/TimePadIntegrationService.cs
+++ DotNetRuServer.Integration/TimePad/TimePadIntegrationService.cs
@@ -1,22 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using DotNetRuServer.Integration.TimePad.Dto;
+using DotNetRuServer.Meetups.BL.Entities;
+using RazorLight;
 
 namespace DotNetRuServer.Integration.TimePad
 {
     public class TimePadIntegrationService
     {
         private readonly IHttpClientFactory _clientFactory;
+        private readonly RazorLightEngine _razorEngine;
 
         public TimePadIntegrationService(IHttpClientFactory factory)
         {
             _clientFactory = factory;
+            var path = Directory.GetCurrentDirectory();
+            _razorEngine = new RazorLightEngineBuilder()
+                .UseFilesystemProject(path)
+                .UseMemoryCachingProvider()
+                .Build();
         }
 
-        public async Task CreateDraftEventAsync(CreateDraftEventDto dto, CancellationToken ct)
+        public async Task CreateDraftEventAsync(Meetup meetup, CancellationToken ct)
         {
             var timePadClient = new TimePadClient(_clientFactory.CreateClient("TimePad"));
             var questions = new List<QuestionInclude>
@@ -27,18 +35,20 @@ namespace DotNetRuServer.Integration.TimePad
                 new QuestionInclude{Field_id = "company", Is_mandatory = false, Name = "Компания"},
                 new QuestionInclude{Field_id = "position", Is_mandatory = false, Name = "Должность"}
             };
+            var htmlDescription = await _razorEngine.CompileRenderAsync("TimePad/EventTemplate.cshtml", new { Name = meetup.Name });
+            
             var createEventBody = new CreateEvent
             {
-                Organization = new OrganizationInclude{Id = dto.OrganizationId, Subdomain = dto.OrganizationSubdomain},
-                Starts_at = dto.StartsAt,
-                Ends_at = dto.EndsAt,
-                Name = dto.EventName,
-                Description_short = dto.ShortDescription,
-                Description_html = dto.HtmlDescription,
-                Location = new LocationInclude {City = dto.City, Address = dto.Address},
+                Organization = new OrganizationInclude{Id = null, Subdomain = null}, //todo: fill
+                Starts_at = DateTimeOffset.MinValue, //todo:fill
+                Ends_at = DateTimeOffset.MinValue, //todo:fill
+                Name = meetup.Name,
+                Description_short = null, //todo: fill
+                Description_html = htmlDescription,
+                Location = new LocationInclude {City = null, Address = meetup.Venue.Address}, //todo: fill
                 Categories = new[] {new CategoryInclude {Id = 452, Name = "ИТ и интернет"}},
                 Access_status = "draft",
-                Tickets_limit = dto.TicketsLimit,
+                Tickets_limit = null, //todo: fill
                 Questions = questions
             };
             
