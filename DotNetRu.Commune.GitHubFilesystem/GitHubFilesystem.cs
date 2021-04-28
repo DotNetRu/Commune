f8ba8ba92e6ce9b6a56f059beea79779f8ea21d5
zetroot <zetroot@gmail.com>
2021-04-28 12:17:57 +0300

    filesystem wip

--- DotNetRu.Commune.GitHubFilesystem/GitHubFilesystem.cs
+++ DotNetRu.Commune.GitHubFilesystem/GitHubFilesystem.cs
@@ -14,6 +14,7 @@ namespace DotNetRu.Commune.GitHubFilesystem
     public class GitHubFilesystem
     {
         private List<GitHubFileStream> files = new List<GitHubFileStream>();
+        private EditingContext? _editingContext;
 
         /// <summary>
         /// Начать сессию работы с файловой системой
@@ -27,10 +28,22 @@ namespace DotNetRu.Commune.GitHubFilesystem
             var client = new GitHubClient(new Connection(new ProductHeaderValue("DotNetRu.Commune.GitHubFilesystem"),
                 GitHubClient.GitHubApiUrl, credStore,
                 new HttpClientAdapter(Net5HttpMessageHandlerFactory.CreateDefault), new SimpleJsonSerializer()));
-            var originalRepo = await client.Repository.Get(originOwner, originRepo);
-            var fork = await client.Repository.Forks.Create(originalRepo.Id, new ());
-            var currentBranch = await client.Git.Reference.CreateBranch(fork.Owner.Login, fork.Name, Guid.NewGuid().ToString("N"));
+
+            var originalRepo = await client.Repository.Get(originOwner, originRepo).ConfigureAwait(false);
+            var originalBranch = await client.Git.Reference.Get(originalRepo.Id, "heads/master").ConfigureAwait(false);
+            var fork = await client.Repository.Forks.Create(originalRepo.Id, new ()).ConfigureAwait(false);
+            var currentBranch = await client.Git.Reference.CreateBranch(fork.Owner.Login, fork.Name, Guid.NewGuid().ToString("N")).ConfigureAwait(false);
+            _editingContext = new EditingContext(client, originalRepo, originalBranch, fork, currentBranch);
         }
 
+        public async Task<IReadOnlyCollection<string>> ListFiles()
+        {
+            if (_editingContext == null) throw new InvalidOperationException();
+            string rootSha = _editingContext.CurrentBranch.Object.Sha;
+            var recursive = await _editingContext.Client.Git.Tree.GetRecursive(_editingContext.LocalRepo.Id, rootSha);
+            var contents = await _editingContext.Client.Repository.Content.GetAllContents(_editingContext.LocalRepo.Id);
+
+
+        }
     }
 }
---
+++ test/DotNetRu.Commune.Test.Fs.GitHubFilesystem/GithubScriptTest.fsx
@@ -0,0 +1,18 @@
+﻿#r "nuget: Octokit"
+open Octokit
+open Octokit.Internal
+
+let ghclient = new GitHubClient(new ProductHeaderValue("FSXCLIENT"), new InMemoryCredentialStore(new Credentials("", AuthenticationType.Bearer)))
+
+let auditContents = ghclient.Repository.Content.GetAllContents("DotNetRu", "Audit")
+                    |> Async.AwaitTask
+                    |> Async.RunSynchronously
+let rec contentMapper (content : RepositoryContentInfo) =
+    match content.Type.Value with
+    | ContentType.File -> seq {content.Path}
+    | ContentType.Dir -> ghclient.Repository.Content.GetAllContents("DotNetRu", "Audit", content.Path) |> Async.AwaitTask |> Async.RunSynchronously |> Seq.collect contentMapper
+    | _ -> Seq.empty
+
+let flattenedCOntents = auditContents |> Seq.map contentMapper |> Seq.collect (fun x -> x)
+
+flattenedCOntents |> Seq.iter (printfn "%s")
