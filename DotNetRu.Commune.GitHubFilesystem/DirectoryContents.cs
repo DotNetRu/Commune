c418cfb9c89788f0c3459cb41870aacbe5f9e7ee
zetroot <zetroot@gmail.com>
2021-04-29 16:17:15 +0300

    me failed with git

---
+++ DotNetRu.Commune.GitHubFilesystem/DirectoryContents.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.FileProviders;
+
+namespace DotNetRu.Commune.GitHubFilesystem
+{
+    public class DirectoryContents : IDirectoryContents
+    {
+        private readonly IReadOnlyCollection<IFileInfo> files;
+
+        public DirectoryContents(IEnumerable<IFileInfo> files)
+        {
+            this.files = files?.ToList() ?? throw new ArgumentNullException(nameof(files));
+        }
+
+        public IEnumerator<IFileInfo> GetEnumerator() => files.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public bool Exists => true;
+    }
+}
--- DotNetRu.Commune.GitHubFilesystem/GitHubFilesystem.cs
+++ DotNetRu.Commune.GitHubFilesystem/GitHubFilesystem.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO.Abstractions;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
 using Octokit;
 using Octokit.Helpers;
 using Octokit.Internal;
@@ -11,9 +13,9 @@ namespace DotNetRu.Commune.GitHubFilesystem
     /// <summary>
     /// Провайдер файлов с github
     /// </summary>
-    public class GitHubFilesystem
+    public class GitHubFilesystem : IFileProvider
     {
-        private List<GitHubFileStream> files = new List<GitHubFileStream>();
+        private EditingContext? _editingContext;
 
         /// <summary>
         /// Начать сессию работы с файловой системой
@@ -24,13 +26,42 @@ namespace DotNetRu.Commune.GitHubFilesystem
         public async Task StartContext(string token, string originRepo, string originOwner)
         {
             var credStore = new InMemoryCredentialStore(new(token, AuthenticationType.Bearer));
-            var client = new GitHubClient(new Connection(new ProductHeaderValue("DotNetRu.Commune.GitHubFilesystem"),
+            var client = new GitHubClient(new Connection(new ProductHeaderValue("DotNetRuCommune"),
                 GitHubClient.GitHubApiUrl, credStore,
                 new HttpClientAdapter(Net5HttpMessageHandlerFactory.CreateDefault), new SimpleJsonSerializer()));
-            var originalRepo = await client.Repository.Get(originOwner, originRepo);
-            var fork = await client.Repository.Forks.Create(originalRepo.Id, new ());
-            var currentBranch = await client.Git.Reference.CreateBranch(fork.Owner.Login, fork.Name, Guid.NewGuid().ToString("N"));
+
+            var originalRepo = await client.Repository.Get(originOwner, originRepo).ConfigureAwait(false);
+            var originalBranch = await client.Git.Reference.Get(originalRepo.Id, "heads/master").ConfigureAwait(false);
+            var fork = await client.Repository.Forks.Create(originalRepo.Id, new ()).ConfigureAwait(false);
+            var currentBranch = await client.Git.Reference.CreateBranch(fork.Owner.Login, fork.Name, Guid.NewGuid().ToString("N")).ConfigureAwait(false);
+            _editingContext = new EditingContext(client, originalRepo, originalBranch, fork, currentBranch);
         }
 
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            if (_editingContext == null) throw new InvalidOperationException();
+            var foundContents = _editingContext.ContentClient.GetAllContents(_editingContext.LocalRepo.Id, subpath)
+                .GetAwaiter().GetResult();
+            if (foundContents.Count == 0) throw new Exception(); //TODO add special file not found exception
+            var content = foundContents.First(); // what shall we do if there are several contents?
+            return new GithubFile(_editingContext, content.Sha, content.Size, content.Path, content.Name,content.Type.Value == ContentType.Dir);
+        }
+
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            if (_editingContext == null) throw new InvalidOperationException();
+            var foundContents = _editingContext.ContentClient.GetAllContents(_editingContext.LocalRepo.Id, subpath)
+                .GetAwaiter().GetResult();
+            return new DirectoryContents(foundContents.Select(FileFactory));
+        }
+
+        private GithubFile FileFactory(RepositoryContent content) =>
+            new(_editingContext ?? throw new InvalidOperationException(),
+                content.Sha,
+                content.Size,
+                content.Path,
+                content.Name,
+                content.Type.Value == ContentType.Dir);
+        public IChangeToken Watch(string filter) => throw new NotSupportedException();
     }
 }
