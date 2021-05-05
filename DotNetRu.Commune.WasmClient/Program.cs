4d36765485c71b71f11c2c0464a6a091f809d60d
zetroot <zetroot@gmail.com>
2021-05-05 20:26:03 +0300

    Logging and configuration (#2)
    * setting up serilog logging
    
    * dependency injection and options injection

--- DotNetRu.Commune.WasmClient/Program.cs
+++ DotNetRu.Commune.WasmClient/Program.cs
@@ -1,5 +1,10 @@
 using System.Threading.Tasks;
+using DotNetRu.Commune.WasmClient.Model;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace DotNetRu.Commune.WasmClient
 {
@@ -10,9 +15,30 @@ namespace DotNetRu.Commune.WasmClient
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddBizLogic();
+            SetupLogging(builder.Logging, builder.Configuration);
+
+            ConfigureServices(builder.Services, builder.Configuration);
 
             await builder.Build().RunAsync();
+        }
+
+        private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<CustomMessage>(configuration.GetSection(nameof(CustomMessage)));
+            services.AddBizLogic();
+        }
+
+        /// <summary>
+        /// Настройка логгирования.
+        /// По умолчанию используется Serilog. Конфигурация логгера задаётся через IConfiguration, из секции "Serilog"
+        /// </summary>
+        /// <param name="builderLogging">Билдер логгера</param>
+        /// <param name="builderConfiguration">Конфигурация</param>
+        private static void SetupLogging(ILoggingBuilder builderLogging, IConfiguration builderConfiguration)
+        {
+            builderLogging.ClearProviders();
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builderConfiguration, "Serilog").CreateLogger();
+            builderLogging.AddSerilog();
         }
     }
 }
