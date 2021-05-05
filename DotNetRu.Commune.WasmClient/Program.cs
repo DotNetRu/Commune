852c25a3012c49953db85f50e4de91bc66b5e6a8
zetroot <zetroot@gmail.com>
2021-05-05 19:54:24 +0300

    setting up serilog logging

--- DotNetRu.Commune.WasmClient/Program.cs
+++ DotNetRu.Commune.WasmClient/Program.cs
@@ -1,5 +1,8 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace DotNetRu.Commune.WasmClient
 {
@@ -10,9 +13,24 @@ namespace DotNetRu.Commune.WasmClient
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            SetupLogging(builder.Logging, builder.Configuration);
+
             builder.Services.AddBizLogic();
 
             await builder.Build().RunAsync();
+        }
+
+        /// <summary>
+        /// Настройка логгирования.
+        /// По умолчанию используется Serilog. Конфигурация логгера задаётся через IConfiguration, из секции "Serilog"
+        /// </summary>
+        /// <param name="builderLogging">Билдер логгера</param>
+        /// <param name="builderConfiguration">Конфигурация</param>
+        private static void SetupLogging(ILoggingBuilder builderLogging, IConfiguration builderConfiguration)
+        {
+            builderLogging.ClearProviders();
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builderConfiguration, "Serilog").CreateLogger();
+            builderLogging.AddSerilog();
         }
     }
 }
