a4fa36955d13142703eb7f41435fbbad79fb780a
zetroot <zetroot@gmail.com>
2021-05-06 20:10:11 +0300

    Логгирование и конфигурация (#142)
    * setting up serilog logging
    
    * dependency injection and options injection
    
    * Update DotNetRu.Commune.WasmClient/Program.cs
    
    Co-authored-by: Igor Labutin <igor@labutin.info>
    
    * finally renamed SetupLogging to ConfigureLogging.
    Fixed typo in xml-doc
    
    Co-authored-by: Igor Labutin <igor@labutin.info>

--- DotNetRu.Commune.WasmClient/Program.cs
+++ DotNetRu.Commune.WasmClient/Program.cs
@@ -1,5 +1,10 @@
 using System.Threading.Tasks;
+using DotNetRu.Commune.WasmClient.Model;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace DotNetRu.Commune.WasmClient
 {
@@ -10,9 +15,35 @@ namespace DotNetRu.Commune.WasmClient
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddBizLogic();
+            ConfigureLogging(builder.Logging, builder.Configuration);
+
+            ConfigureServices(builder.Services, builder.Configuration);
 
             await builder.Build().RunAsync();
+        }
+
+        /// <summary>
+        /// Конфигурация DI-контейнера
+        /// </summary>
+        /// <param name="services">Коллекция служб - собственно контейнер</param>
+        /// <param name="configuration">Конфигурация</param>
+        private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<CustomMessage>(configuration.GetSection(nameof(CustomMessage)));
+            services.AddBizLogic();
+        }
+
+        /// <summary>
+        /// Настройка логирования.
+        /// По умолчанию используется Serilog. Конфигурация логгера задаётся через IConfiguration, из секции "Serilog"
+        /// </summary>
+        /// <param name="builderLogging">Билдер логгера</param>
+        /// <param name="builderConfiguration">Конфигурация</param>
+        private static void ConfigureLogging(ILoggingBuilder builderLogging, IConfiguration builderConfiguration)
+        {
+            builderLogging.ClearProviders();
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builderConfiguration, "Serilog").CreateLogger();
+            builderLogging.AddSerilog();
         }
     }
 }
