72fefb65efcf0d0ff7f1b226190d23750bcaf408
zetroot <zetroot@gmail.com>
2021-06-15 23:27:33 +0300

    added github filesystem implementation

---
+++ DotNetRu.Commune.GithubFilesystem/EditingContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace DotNetRu.Commune.GithubFilesystem
+{
+    /// <summary>
+    /// Represents a single session of work with filesystem. Actually - main part of it is a link to current branch of forked repository.
+    /// </summary>
+    internal class EditingContext
+    {
+        /// <summary>
+        /// GitHub API client instance
+        /// </summary>
+        public IGitHubClient Client { get; }
+
+        /// <summary>
+        /// Sub-client for accessing API for manipulation with repository contents
+        /// </summary>
+        public virtual IRepositoryContentsClient ContentClient => Client.Repository.Content;
+
+        /// <summary>
+        /// Sub-client for accessing API for manipulation with pull requests
+        /// </summary>
+        public virtual IPullRequestsClient PullRequestsClient => Client.PullRequest;
+
+        /// <summary>
+        /// Original repository
+        /// </summary>
+        public Repository OriginRepo { get; }
+
+        /// <summary>
+        /// Original (source) branch
+        /// </summary>
+        /// <remarks>HINT: It is "master"</remarks>
+        public Reference OriginBranch { get; }
+
+        /// <summary>
+        /// Users forl of original repo
+        /// </summary>
+        public Repository LocalRepo { get; }
+
+        /// <summary>
+        /// Current branch. All files are editing in it.
+        /// </summary>
+        public Reference CurrentBranch { get; }
+
+        /// <summary>
+        /// Commit the changes.
+        /// Creates a pr from <see cref="CurrentBranch"/> of <see cref="LocalRepo"/> to <see cref="OriginBranch"/> of <see cref="OriginRepo"/>
+        /// </summary>
+        public Task Commit()
+        {
+            var head = $"{LocalRepo.Owner.Login}:{CurrentBranch.Ref}";
+            return PullRequestsClient.Create(OriginRepo.Id,
+                new("AUTOMATED PR", head, OriginBranch.Ref) {Draft = true});
+        }
+
+        public EditingContext(IGitHubClient client, Repository originRepo, Reference originBranch, Repository localRepo,
+            Reference currentBranch)
+        {
+            Client = client ?? throw new ArgumentNullException(nameof(client));
+            OriginRepo = originRepo ?? throw new ArgumentNullException(nameof(originRepo));
+            OriginBranch = originBranch ?? throw new ArgumentNullException(nameof(originBranch));
+            LocalRepo = localRepo ?? throw new ArgumentNullException(nameof(localRepo));
+            CurrentBranch = currentBranch ?? throw new ArgumentNullException(nameof(currentBranch));
+        }
+    }
+}
---
+++ DotNetRu.Commune.GithubFilesystem/GithubFileSystem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DotNetRu.Auditor.Storage.FileSystem;
+using Octokit;
+using Octokit.Helpers;
+using Octokit.Internal;
+
+namespace DotNetRu.Commune.GithubFilesystem
+{
+    /// <summary>
+    /// Virtual filesystem implementation using GitHub repositories for storing files. Implements <see cref="IFileSystem"/>
+    /// </summary>
+    public class GithubFileSystem : IFileSystem
+    {
+        private const string ProductName = "DotNetRuCommune";
+        private EditingContext? _editingContext;
+
+        /// <summary>
+        /// Begin work with filesytem. Creates a fork of original repository, than creates a new branch in this fork and
+        /// stores all the data inside editing context. Without calling this method you can't do anything else - enumerate, read, create
+        /// </summary>
+        /// <param name="token">Personal access token</param>
+        /// <param name="originRepo">Original repository name</param>
+        /// <param name="originOwner">Original repository owner (user or organization)</param>
+        public async Task StartContext(string token, string originRepo, string originOwner)
+        {
+            var credStore = new InMemoryCredentialStore(new(token, AuthenticationType.Bearer));
+            var client = new GitHubClient(new Connection(new ProductHeaderValue(ProductName),
+                GitHubClient.GitHubApiUrl, credStore,
+                new HttpClientAdapter(Net5HttpMessageHandlerFactory.CreateDefault),
+                new SimpleJsonSerializer()));
+
+            var originalRepo = await client.Repository.Get(originOwner, originRepo).ConfigureAwait(false);
+            var originalBranch = await client.Git.Reference.Get(originalRepo.Id, "heads/master").ConfigureAwait(false);
+            var fork = await client.Repository.Forks.Create(originalRepo.Id, new ()).ConfigureAwait(false);
+            var currentBranch = await client.Git.Reference.CreateBranch(fork.Owner.Login, fork.Name, Guid.NewGuid().ToString("N")).ConfigureAwait(false);
+            _editingContext = new EditingContext(client, originalRepo, originalBranch, fork, currentBranch);
+        }
+
+        /// <inheritdoc />
+        public string Name => "/";
+
+        /// <inheritdoc />
+        public string FullName => "/";
+
+        /// <inheritdoc />
+        public bool Exists => true;
+
+        /// <inheritdoc />
+        public ValueTask<IDirectory> GetDirectoryInfoAsync(string subPath) => throw new System.NotImplementedException();
+
+        /// <inheritdoc />
+        public ValueTask<IFile> GetFileInfoAsync(string subPath) => throw new System.NotImplementedException();
+
+        /// <inheritdoc />
+        public IAsyncEnumerable<IDirectory> EnumerateDirectoriesAsync() => throw new System.NotImplementedException();
+
+        /// <inheritdoc />
+        public IAsyncEnumerable<IFile> EnumerateFilesAsync() => throw new System.NotImplementedException();
+
+        /// <inheritdoc />
+        public ValueTask<IFile> CreateFileAsync(string subPath) => throw new System.NotImplementedException();
+
+        /// <inheritdoc />
+        public ValueTask DeleteFileAsync(string subPath) => throw new System.NotImplementedException();
+    }
+}
