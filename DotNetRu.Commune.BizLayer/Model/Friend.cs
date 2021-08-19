f5a586fd7056c030562d0fc96f2e5e190d5af16f
zetroot <zetroot@gmail.com>
2021-08-19 12:35:59 +0300

    added friend entity

---
+++ DotNetRu.Commune.BizLayer/Model/Friend.cs
@@ -0,0 +1,11 @@
+namespace DotNetRu.Commune.BizLayer.Model
+{
+    /// <summary>
+    /// Friend entity
+    /// </summary>
+    /// <param name="Id">Friend identifier</param>
+    /// <param name="Name">Friend name</param>
+    /// <param name="Url">Friend website</param>
+    /// <param name="Description">Friend description</param>
+    public record Friend(string? Id, string? Name, string? Url, string? Description);
+}
