6bace7a5b1659d6f8923f0e74a9039b9183e7dcb
zetroot <zetroot@gmail.com>
2021-08-20 10:25:06 +0300

    added speaker entity

---
+++ DotNetRu.Commune.BizLayer/Model/Speaker.cs
@@ -0,0 +1,27 @@
+namespace DotNetRu.Commune.BizLayer.Model
+{
+    /// <summary>
+    /// Speaker entity
+    /// </summary>
+    /// <param name="Id">Speaker's identitier</param>
+    /// <param name="Name">Speaker's name</param>
+    /// <param name="CompanyName">Speaker's company, if present</param>
+    /// <param name="CompanyUrl">Speaker's company website, if present</param>
+    /// <param name="Description">Spekaer's description</param>
+    /// <param name="BlogUrl">Speaker's blog url</param>
+    /// <param name="ContactsUrl">Speaker's contans page url</param>
+    /// <param name="TwitterUrl">Link to speaker's twitter account</param>
+    /// <param name="HabrUrl">Link to speaker's account at habr.com</param>
+    /// <param name="GitHubUrl">Link to speaker's account at github.com</param>
+    public record Speaker(string? Id,
+        string? Name,
+        string? CompanyName,
+        string? CompanyUrl,
+        string? Description,
+        string? BlogUrl,
+        string? ContactsUrl,
+        string? TwitterUrl,
+        string? HabrUrl,
+        string? GitHubUrl);
+
+}
