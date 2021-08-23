f7640c23b2546a2c87a44077833c89c03521a488
zetroot <zetroot@gmail.com>
2021-08-23 13:49:22 +0200

    Авторизация и старт контекста редактирования (#145)
    * added proroject for github in-memory filesystem implementation
    
    Configured max warning level, treat warnings as errors, turned nullable reference type feture on
    
    * added DotNetRu filesystem abstractions
    
    This code is copied from DotNetRu/Auditor repository. It will remain here until Auditor NuGet package will be ready for release.
    It should be removed after release and all implementations should reference DotNetRu.Auditor nuget for abstractions
    
    * added octokit nuget package
    
    * custom implementation of http client handler factory for using in NET5 for Octokit
    
    * generating xml-documentation for the project
    
    * Blank documentation for abstractions
    
    The docs are blank only because this code will be replaced soon
    
    * added github filesystem implementation
    
    * Added source repository settings in appsettings.json.
    They are read by the app and stored in DI container
    
    * Added reference to GithubFilesystem project
    
    * registered github filesystem in DI-container
    
    * removed demo custom message class
    
    * hide http handler factory to internals
    
    * context start from blazor
    
    * Removed temp interface code.
    Referenced interfaces from Auditor nuget package.
    
    * after-review fixes
    
    * fixed typos

---
+++ DotNetRu.Commune.GithubFilesystem/EditingContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace DotNetRu.Commune.GithubFileSystem
+{
+    /// <summary>
+    /// Represents a single session of work with filesystem. Actually - main part of it is a link to current branch of forked repository.
+    /// </summary>
+    internal class EditingContext
+    {
+        /// <summary>
+        /// GitHub API client instance
+        /// </summary>
+        public IGitHubClient Client { get; }
+
+        /// <summary>
+        /// Sub-client for accessing API for manipulation with repository contents
+        /// </summary>
+        public virtual IRepositoryContentsClient ContentClient => Client.Repository.Content;
+
+        /// <summary>
+        /// Sub-client for accessing API for manipulation with pull requests
+        /// </summary>
+        public virtual IPullRequestsClient PullRequestsClient => Client.PullRequest;
+
+        /// <summary>
+        /// Original repository
+        /// </summary>
+        public Repository OriginRepo { get; }
+
+        /// <summary>
+        /// Original (source) branch
+        /// </summary>
+        /// <remarks>HINT: It is "master"</remarks>
+        public Reference OriginBranch { get; }
+
+        /// <summary>
+        /// User's fork of original repo
+        /// </summary>
+        public Repository LocalRepo { get; }
+
+        /// <summary>
+        /// Current branch. All files are editing in it.
+        /// </summary>
+        public Reference CurrentBranch { get; }
+
+        /// <summary>
+        /// Commit the changes.
+        /// Creates a pr from <see cref="CurrentBranch"/> of <see cref="LocalRepo"/> to <see cref="OriginBranch"/> of <see cref="OriginRepo"/>
+        /// </summary>
+        /// <param name="prTitle">Title of the creating pull request</param>
+        public Task Commit(string prTitle)
+        {
+            if (string.IsNullOrWhiteSpace(prTitle)) throw new ArgumentException("Pull request title must be not empty.");
+            var head = $"{LocalRepo.Owner.Login}:{CurrentBranch.Ref}";
+            return PullRequestsClient.Create(OriginRepo.Id,
+                new(prTitle, head, OriginBranch.Ref) {Draft = true});
+        }
+
+        public EditingContext(IGitHubClient client, Repository originRepo, Reference originBranch, Repository localRepo,
+            Reference currentBranch)
+        {
+            Client = client ?? throw new ArgumentNullException(nameof(client));
+            OriginRepo = originRepo ?? throw new ArgumentNullException(nameof(originRepo));
+            OriginBranch = originBranch ?? throw new ArgumentNullException(nameof(originBranch));
+            LocalRepo = localRepo ?? throw new ArgumentNullException(nameof(localRepo));
+            CurrentBranch = currentBranch ?? throw new ArgumentNullException(nameof(currentBranch));
+        }
+    }
+}
---
+++ DotNetRu.Commune.GithubFilesystem/Net5HttpMessageHandlerFactory.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Http;
+
+namespace DotNetRu.Commune.GithubFileSystem
+{
+    internal class Net5HttpMessageHandlerFactory
+    {
+        public static HttpMessageHandler CreateDefault()
+        {
+            return CreateDefault(null);
+        }
+
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "proxy")]
+        [SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+        public static HttpMessageHandler CreateDefault(IWebProxy? proxy)
+        {
+            var handler = new HttpClientHandler();
+
+            if (handler.SupportsAutomaticDecompression)
+            {
+                handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            }
+
+            if (handler.SupportsProxy && proxy != null)
+            {
+                handler.UseProxy = true;
+                handler.Proxy = proxy;
+            }
+
+            return handler;
+        }
+    }
+}
